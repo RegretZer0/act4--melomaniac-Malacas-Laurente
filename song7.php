074502c7d89d8c19b83da5d32c324b9a929d0f14
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:35:28 +0800

    song7.php added

---
+++ song7.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Di Na Kita Mahal</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+	<?php
+	echo "<pre>";
+	// Title
+	echo "<strong>Di Na Kita Mahal</strong>";
+	echo "\nBy: Silent Sanctuary";
+
+	echo "<div class='chords'>\n \n \n</div>";
+	echo "CAPO 3rd Fret";
+
+	// Verse 1
+	echo "\n[Verse 1]";
+	echo "<div class='chords'>               G    Em                 Bm           Em</div>";
+	echo "Hindi na kita mahal, 'yan ba ang nais mong marinig?";
+	echo "<div class='chords'>               G      Em                   Bm         Em</div>";
+	echo "Sa hangin naka-sandal, nagbabadya na ang huling halik";
+
+	// Pre-Chorus 1
+	echo "\n\n[Pre-Chorus 1]";
+	echo "<div class='chords'>         D             Em       Bm         D - Am</div>";
+	echo "Paano na ang mga gabi, 'kung 'di ka katabi";
+
+	// Chorus
+	echo "\n\n[Chorus]";
+	echo "<div class='chords'>G              Bm              Am         D</div>";
+	echo "Kung ako lang, kung ako lang ay 'di ko sasabihin";
+	echo "<div class='chords'>        G           Bm            Am           D      Cm   D</div>";
+	echo "Para sa'yo, para sa'yo kailangan kang limutin";
+
+	// Verse 2
+	echo "\n\n[Verse 2]";
+	echo "<div class='chords'>            G          Em              Bm       Em</div>";
+	echo "Sumpaang magtatagal, mga pangarap naging abo";
+	echo "<div class='chords'>             G        Em              Bm       Em</div>";
+	echo "Sino nga ba ang hangal?, Bakit tayo nagkaganito?";
+
+	// Pre-Chorus 2
+	echo "\n\n[Pre-Chorus 2]";
+	echo "<div class='chords'>     D               Em    Bm            Am  Cm</div>";
+	echo "Wala na bang halaga, mundo nating dalawa";
+
+	// Chorus
+	echo "\n\n[Chorus]";
+	echo "<div class='chords'>G              Bm              Am         D </div>";
+	echo "Kung ako lang, kung ako lang ay 'di ko sasabihin";
+	echo "<div class='chords'>        G           Bm            Am           D      Cm  G</div>";
+	echo "Para sa'yo, para sa'yo kailangan kang limutin";
+	echo "<div class='chords'>                                             Cm  Bm  Em  Am  Bm  Em  Cm  Bm  Em  Am  Cm Dm Ebm</div>";
+	echo "Limutin...";
+
+	// Bridge
+	echo "\n\n[Bridge]";
+	echo "<div class='chords'>         G            Em         D(pause)</div>";
+	echo "Kung ako lang, para sa'yo";
+
+	// Chorus
+	echo "\n\n[Chorus]";
+	echo "<div class='chords'>G#            Cm               C#         Eb </div>";
+	echo "Kung ako lang, kung ako lang ay 'di ko sasabihin";
+	echo "<div class='chords'>G#          Cm         C#            Eb </div>";
+	echo "Para sa'yo, para sa'yo handa kitang limutin";
+	echo "\n\n<div class='chords'>G#            Cm               C#         Eb </div>";
+	echo "Kung ako lang, kung ako lang ay 'di ko kakayanin";
+	echo "<div class='chords'>G#          Cm         C#            Eb </div>";
+	echo "Para sa'yo, dahil sa'yo ay aking pipilitin";
+
+	// Outro
+	echo "\n\n[Outro]";
+	echo "<div class='chords'>      G#      Eb   Fm</div>";
+	echo "Buhay ko'y isusugal";
+	echo "<div class='chords'>     Cm       D     Fm</div>";
+	echo "Ikaw man ang nasasaktan";
+	echo "<div class='chords'>C      D     G</div>";
+	echo "     Di Na Kita Mahal";
+	?>
+	</div>
+</body>
+</html>
