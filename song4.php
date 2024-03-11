05cdb0c5e574a75aea218f269ad59d4345d5b52c
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:33:27 +0800

    song4.php added

---
+++ song4.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Teka Lang</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+	<?php
+
+	echo "<pre>";
+	// TITLE
+	echo "<strong>Teka Lang</strong>";
+	echo "<br>";
+	echo "by Emman Nimedez";
+	echo "<br><br><br>";
+
+	// INTRO
+	echo "<div class='chords'>    DM7\t\t\tBm7\t\tF#m7\tAm7   D7  </div>";
+	echo "Pabagalin muna natin ang ikot ng mundo";
+	echo "<br>";
+	echo "<div class='chords'>    DM7\t\t\tBm7\t\tF#m7\tAm7   D7  </div>";
+	echo "Pahintuin mga kamay ng oras sa relo";
+	echo "<br>";
+	echo "<div class='chords'>  DM7\t\t</div>";
+	echo" Dahan dahan";
+	echo "<br>";
+	echo "<div class='chords'>  Bm7\t\t\t</div>";
+	echo "Dahan dahan lang";
+	echo "<br>";
+	echo "<div class='chords'> F#m7\t\t</div>";
+	echo "Dahan dahan";
+	echo "<br>";
+	echo "<div class='chords'>  Am7\t  D7\t\t</div>";
+	echo "Dahan dahan lang";
+	echo "<br><br><br>";
+
+	// VERSE 1
+	echo "<div class='chords'> DM7\t\t\t</div>";
+	echo "Sobrang saya dahil";
+	echo "<br>";
+	echo "<div class='chords'>Bm7\t\t\t</div>";
+	echo "Nandito ka na kahit";
+	echo "<br>";
+	echo "<div class='chords'> F#m7\t\t</div>";
+	echo "Mamaya lang";
+	echo "<br>";
+	echo "<div class='chords'>   Am7\t   D7 </div>";
+	echo "Ay aalis ka na rin agad";
+	echo "<br>";
+	echo "<div class='chords'>DM7\t\t\t</div>";
+	echo "Wag ka sanang mainis";
+	echo "<br>";
+	echo "<div class='chords'>Bm7\t\t\t </div>";
+	echo "Panahon natin mabilis";
+	echo "<br>";
+	echo "<div class='chords'>F#m7\t\t\t  </div>";
+	echo "Sandal mo muna sandali";
+	echo "<br>";
+	echo "<div class='chords'>   Am7\t\tD7\t\t      </div>";
+	echo "Di naman nagmamadali, di'ba?";
+	echo "<br><br><br>";
+
+	// VERSE 2
+	echo "<div class='chords'> DM7\t\t\t\t</div>";
+	echo "Dantay ka muna sakin beh";
+	echo "<br>";
+	echo "<div class='chords'>Bm7\t\t\t   </div>";
+	echo "Sulitin natin ang gabi";
+	echo "<br>";
+	echo "<div class='chords'> F#m7\t\t\t\t</div>";
+	echo "Iyong mga problema'y itabi";
+	echo "<br>";
+	echo "<div class='chords'>   Am7\t\tD7\t\t</div>";
+	echo "Labi mo sa akin idampi";
+	echo "<br><br><br>";
+
+	// PRE CHORUS
+	echo "<div class='chords'>DM7\t\t\t</div>";
+	echo "At kahit anong lalim";
+	echo "<br>";
+	echo "<div class='chords'>Bm7\t\t  </div>";
+	echo "Aking sisisirin";
+	echo "<br>";
+	echo "<div class='chords'> F#m7\t\t\t</div>";
+	echo "Hindi na mabibitin";
+	echo "<br>";
+	echo "<div class='chords'>Am7\t   D7\t  </div>";
+	echo "Pa sa aking gagawin";
+	echo "<br><br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>    DM7\t\t    Bm7\t\t\t\tF#m7  </div>";
+	echo "Pabagalin muna natin ang ikot ng mundo";
+	echo "<br>";
+	echo "<div class='chords'>Am7\t\t\tD7\t\t</div>";
+	echo "Ako'y sasayaw sa himig mo atin ang gabi na 'to";
+	echo "<br>";
+	echo "<div class='chords'>  DM7\t\t   Bm7\t\t\t F#m7  </div>";
+	echo "Pahintuin mga kamay ng oras sa relo";
+	echo "<br>";
+	echo "<div class='chords'>Am7\t\t\tD7\t\t</div>";
+	echo "Takasan natin ang gulo, lilipad papalayo";
+	echo "<br>";
+	echo "<div class='chords'>  DM7\t\t</div>";
+	echo" Dahan dahan";
+	echo "<br>";
+	echo "<div class='chords'>  Bm7\t\t\t</div>";
+	echo "Dahan dahan lang";
+	echo "<br>";
+	echo "<div class='chords'> F#m7\t\t</div>";
+	echo "Dahan dahan";
+	echo "<br>";
+	echo "<div class='chords'>  Am7\t  D7\t\t</div>";
+	echo "Dahan dahan lang";
+	echo "<br><br><br>";
+
+	// VERSE 3
+	echo "<div class='chords'>DM7\t\t\t\t</div>";
+	echo "Dahan dahan ka lang ngayon";
+	echo "<br>";
+	echo "<div class='chords'>Bm7\t\t  F#m7\t\t\t  </div>";
+	echo "Alam ko naman hanggang dito ka lang";
+	echo "<br>";
+	echo "<div class='chords'>    Am7\t\tD7\t </div>";
+	echo "Kalaban ang orasan";
+	echo "<br>";
+	echo "<div class='chords'>DM7\t\t\t</div>";
+	echo "Paano na ko pag gabi";
+	echo "<br>";
+	echo "<div class='chords'> Bm7\t\t\t\t</div>";
+	echo "Di sanay ng walang katabi";
+	echo "<br>";
+	echo "<div class='chords'>F#m7\t\t\t  </div>";
+	echo "Sandal mo muna sandali";
+	echo "<br>";
+	echo "<div class='chords'>Am7\t\tD7\t\t</div>";
+	echo "Di naman nagmamadali, kaya";
+	echo "<br><br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>    DM7\t\t    Bm7\t\t\t\tF#m7  </div>";
+	echo "Pabagalin muna natin ang ikot ng mundo";
+	echo "<br>";
+	echo "<div class='chords'>Am7\t\t\tD7\t\t</div>";
+	echo "Ako'y sasayaw sa himig mo atin ang gabi na 'to";
+	echo "<br>";
+	echo "<div class='chords'>  DM7\t\t   Bm7\t\t\t F#m7  </div>";
+	echo "Pahintuin mga kamay ng oras sa relo";
+	echo "<br>";
+	echo "<div class='chords'>Am7\t\t\tD7\t\t</div>";
+	echo "Takasan natin ang gulo, lilipad papalayo";
+	echo "<br>";
+	echo "<div class='chords'>  DM7\t\t</div>";
+	echo" Dahan dahan";
+	echo "<br>";
+	echo "<div class='chords'>  Bm7\t\t\t</div>";
+	echo "Dahan dahan lang";
+	echo "<br>";
+	echo "<div class='chords'> F#m7\t\t</div>";
+	echo "Dahan dahan";
+	echo "<br>";
+	echo "<div class='chords'>  Am7\t  D7\t\t</div>";
+	echo "Dahan dahan lang";
+	echo "<br><br><br>";
+
+	// OUTRO
+	echo "<div class='chords'> DM7   Bm7\t\t\t  F#m7\t\t</div>";
+	echo "Ohhh pwede bang magpahinga muna saglit";
+	echo "<br>";
+	echo "<div class='chords'>  Am7\t  D7\t\t</div>";
+	echo "Dahan dahan lang";
+	echo "<br>";
+	echo "<div class='chords'> DM7   Bm7\t\t\t  F#m7\t\t</div>";
+	echo "Ohhh pwede bang magpahinga muna saglit";
+	echo "<br>";
+	echo "<div class='chords'>  Am7\t  D7\t\t</div>";
+	echo "Dahan dahan lang ";
+	echo "</pre>";
+	?>
+	</div>
+</body>
+</html>
