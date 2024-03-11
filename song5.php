25ebab70df8c5ac25639224ded218d67c89df947
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:34:07 +0800

    song5.php added

---
+++ song5.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Himala</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+	<?php
+	// TITLE
+	echo "<strong>Himala</strong>";
+	echo "<br>";
+	echo "by Rivermaya";
+	echo "<br><br><br>";
+
+	// VERSE 1
+	echo "<pre>";
+	echo "<div class='chords'>F#m\t   </div>";
+	echo "Pangarap ko'y";
+	echo "<br>";
+	echo "<div class='chords'>G\t   </div>";
+	echo "Makita kang";
+	echo "<br>";
+	echo "<div class='chords'>      D\t\t\tD9</div>";
+	echo "Naglalaro sa buwan<br>";
+	echo "<div class='chords'>F#m   </div>";
+	echo "Inalay mo<br>";
+	echo "<div class='chords'>G\t     </div>";
+	echo "Sa akin ang";
+	echo "<br>";
+	echo "<div class='chords'> D\t\t\t   D9</div>";
+	echo "Gabing walang hangganan";
+	echo "<br>";
+	echo "<div class='chords'>    F#m\t\t      G\t\t   D\tD9</div>";
+	echo "Hindi mahanap sa lupa ang pag-asa";
+	echo "<br>";
+	echo "<div class='chords'>    F#m\t\t G\t</div>";
+	echo "Nakikiusap nalang";
+	echo "<br><br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>\t   D\tG\t\t     </div>";
+	echo "Himala, kasalanan bang";
+	echo "<br>";
+	echo "<div class='chords'>F#m\t\t\t     </div>";
+	echo "Humingi ako sa langit ng";
+	echo "<br>";
+	echo "<div class='chords'>G\t  D\t G\t\t</div>";
+	echo "Isang himala, kasalanan bang";
+	echo "<br>";
+	echo "<div class='chords'>F#m\t\t\t     </div>";
+	echo "Humingi ako sa langit ng";
+	echo "<br>";
+	echo "<div class='chords'>\tD\t  G\t    </div>";
+	echo "Isang himala";
+	echo "<br><br><br>";
+
+	// VERSE 2
+	echo "<div class='chords'>   F#m\t\t</div>";
+	echo "Pangarap ko'y";
+	echo "<br>";
+	echo "<div class='chords'>   G\t\t</div>";
+	echo "Makita ang";
+	echo "<br>";
+	echo "<div class='chords'>\t D\t\t  D9\t</div>";
+	echo "Liwanag ng umaga";
+	echo "<br>";
+	echo "<div class='chords'> F#m\t</div>";
+	echo "Naglalambing";
+	echo "<br>";
+	echo "<div class='chords'>\tG\t    D\tD9 </div>";
+	echo "Sa iyong mga mata";
+	echo "<br>";
+	echo "<div class='chords'>F#m\t\t   G\t    D\tD9 </div>";
+	echo "Di mahagilap sa lupa ang pag-asa";
+	echo "<br>";
+	echo "<div class='chords'>   F#m\t\t  G\t</div>";
+	echo "Nakikiusap sa buwan";
+	echo "<br><br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>\t   D\tG\t\t     </div>";
+	echo "Himala, kasalanan bang";
+	echo "<br>";
+	echo "<div class='chords'>F#m\t\t\t     </div>";
+	echo "Humingi ako sa langit ng";
+	echo "<br>";
+	echo "<div class='chords'>G\t  D\t G\t\t</div>";
+	echo "Isang himala, kasalanan bang";
+	echo "<br>";
+	echo "<div class='chords'>F#m\t\t\t     </div>";
+	echo "Humingi ako sa langit ng";
+	echo "<br>";
+	echo "<div class='chords'>\tD\t  G\t    </div>";
+	echo "Isang himala";
+	echo "<br><br><br>";
+
+	// INTERLUDE	
+	echo "<div class='chords'>AmEmFF 2x</div>";
+	echo "<br><br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>\t   D\tG\t\t     </div>";
+	echo "Himala, kasalanan bang";
+	echo "<br>";
+	echo "<div class='chords'>F#m\t\t\t     </div>";
+	echo "Humingi ako sa langit ng";
+	echo "<br>";
+	echo "<div class='chords'>G\t  D\t G\t\t</div>";
+	echo "Isang himala, kasalanan bang";
+	echo "<br>";
+	echo "<div class='chords'>F#m\t\t\t     </div>";
+	echo "Humingi ako sa langit ng";
+	echo "<br>";
+	echo "<div class='chords'>\tD\t  G\t    </div>";
+	echo "Isang himala";
+	echo "<br><br><br>";
+
+	echo "<div class='chords'>\t   D\tG\t\t     </div>";
+	echo "Himala, kasalanan bang";
+	echo "<br>";
+	echo "<div class='chords'>F#m\t\t\t     </div>";
+	echo "Humingi ako sa langit ng";
+	echo "<br>";
+	?>
+	</div>
+</body>
+</html>
