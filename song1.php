13998f67f4ee7ccd4056859a52e03919a5351a4c
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:31:46 +0800

    song1.php added

---
+++ song1.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ghetto</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+		<?php
+
+		echo "<pre>";
+		// Title
+		echo "<strong>Ghetto</strong>";
+		echo "<br>";
+		echo "by Akon";
+		echo "<br><br><br><br>";
+
+		// Intro
+		echo "<div class='chords'>Gm\tF</div>";
+		echo "<div class='chords'>Gm   F \t     Gm  </div>";
+		echo "Ghetto, ghetto";
+		echo "<br>";
+		echo "<div class='chords'>  F\t  Gm\t F</div>";
+		echo "Ghetto, Ghetto livin'";
+		echo "<br><br><br>";
+
+		// VERSE 1
+		echo "<div class='chords'>Gm\t\t\t\t F\t   </div>";
+		echo "These streets remind me of quicksand";
+		echo "<br>";
+		echo "<div class='chords'> \t\tGm\t\t\t\tF</div>";
+		echo "When you're on it, you'll keep goin' down";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t  </div>";
+		echo "And there's no one to hold on to";
+		echo "<br>";
+		echo "<div class='chords'>F\t    </div>";
+		echo "And there's no one to pull you out";
+		echo "<br>";
+		echo "<div class='chords'>\t\tGm\t\t\t\t   F\t  </div>";
+		echo "You keep on fallin', no one can hear you callin'";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t  Gm</div>";
+		echo "So you end up self-destructin'";
+		echo "<br><br>";
+		echo "On the corner with the toolie on the waist line";
+		echo "<br>";
+		echo "<div class='chords'>F\t\t\t\t\t       Gm </div>";
+		echo "Just got up out the thing doin' state time";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t\t\t F</div>";
+		echo "Teeth marks on my back from the K-9";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t\t\t    Gm</div>";
+		echo "Dark memories of when there was no sunshine";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t\tF</div>";
+		echo "'Cause they said that I wouldn't make it";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t Gm</div>";
+		echo "I remember like yesterday";
+		echo "<br>";
+		echo "<div class='chords'>\t\t    F</div>";
+		echo "Holdin' on to what God gave me";
+		echo "<br><br><br>";
+
+		// CHORUS
+		echo "'Cause that's the life";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\t\t </div>";
+		echo "When you're livin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>F\t\t\t\t</div>";
+		echo "And eatin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\tF </div>";
+		echo "Oh, sleepin' in the (ghetto)";
+		echo "<br><br>";
+		echo "Ghetto, that's the life";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\t\t </div>";
+		echo "When you're livin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>F\t\t\t\t</div>";
+		echo "And eatin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>  Gm\t\t\t\t\t  F\t</div>";
+		echo "Oh, sleepin' in the (ghetto)(ghetto)";
+		echo "<br><br><br>";
+
+		// VERSE 2
+		echo "<div class='chords'>Gm\t\t\tF\t</div>";
+		echo "No need to cherish luxuries";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t\t  Gm</div>";
+		echo "'Cause everything come and go";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t\tF    </div>";
+		echo "Even the life that you have is borrowed";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t\t  Gm</div>";
+		echo "'Cause you're not promised tomorrow";
+		echo "<br>";
+		echo "<div class='chords'>\t\t F\t\t\t</div>";
+		echo "So live your life as if every day is gon' be your last";
+		echo "<br>";
+		echo "<div class='chords'> Gm\t\t\t\t</div>";
+		echo "Once you've moved forward";
+		echo "<br>";
+		echo "<div class='chords'>   F\t\t\t\t\t\t\t</div>";
+		echo "You can't go back just to remove your past";
+		echo "<br>";
+		echo "<div class='chords'> Gm\t\t\t\t\t  </div>";
+		echo "'Cause you gotta be willin' to pray";
+		echo "<br>";
+		echo "<div class='chords'>\t\tF\t\t\t\t\t  Gm  </div>";
+		echo "Yes, there gotta be, there gotta be a better way";
+		echo "<br><br>";
+		echo "Yes, you gotta be willin' to pray";
+		echo "<br>";
+		echo "<div class='chords'>\t\t F\t\t\t\t\t   Gm  </div>";
+		echo "'Cause there gotta be, there gotta be a better day";
+		echo "<br>";
+		echo "<div class='chords'>\t\t\t\t   F</div>";
+		echo "Whoever said that the struggle would stop today?";
+		echo "<br>";
+		echo "<div class='chords'>\t\tGm</div>";
+		echo "A lot a niggas dead or locked away";
+		echo "<br>";
+		echo "<div class='chords'>\t\tF  </div>";
+		echo "Teenage women growin' up with AIDS";
+		echo "<br><br><br>";
+
+		// CHORUS
+		echo "'Cause that's the life";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\t\t </div>";
+		echo "When you're livin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>F\t\t\t\t</div>";
+		echo "And eatin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\tF </div>";
+		echo "Oh, sleepin' in the (ghetto)";
+		echo "<br><br>";
+		echo "Ghetto, that's the life";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\t\t </div>";
+		echo "When you're livin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>F\t\t\t\t</div>";
+		echo "And eatin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>  Gm\t\t\t\t\t  F\t</div>";
+		echo "Oh, sleepin' in the (ghetto)(ghetto)";
+		echo "<br><br><br>";
+
+		// BRIDGE
+		echo "<div class='chords'>Gm\t\t\t\t</div>";
+		echo "Gun shots every night in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'> F\t\t\t</div>";
+		echo "Crooked cops on sight in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'> Gm\t\t\t\t</div>";
+		echo "Everyday is a fight in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'> F\t\t\t\t</div>";
+		echo "Oh oh oh oh oh (ghetto livin')";
+		echo "<br>";
+		echo "<div class='chords'> Gm\t\t\t\t</div>";
+		echo "Got kids to feed in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>  F\t\t\t\t</div>";
+		echo "Sellin' coke and weed in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\t   </div>";
+		echo "Everyday somebody bleed in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'> F\t\t\t\t</div>";
+		echo "Oh oh oh oh oh (ghetto livin')";
+		echo "<br><br><br>";
+
+		// CHORUS
+		echo "'Cause that's the life";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\t\t </div>";
+		echo "When you're livin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>F\t\t\t\t</div>";
+		echo "And eatin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\tF </div>";
+		echo "Oh, sleepin' in the (ghetto)";
+		echo "<br><br>";
+		echo "Ghetto, that's the life";
+		echo "<br>";
+		echo "<div class='chords'>Gm\t\t\t\t\t </div>";
+		echo "When you're livin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>F\t\t\t\t</div>";
+		echo "And eatin' in the (ghetto)";
+		echo "<br>";
+		echo "<div class='chords'>  Gm\t\t\t\t\t  F\t</div>";
+		echo "Oh, sleepin' in the (ghetto)(ghetto)";
+		echo "</pre>";
+
+		?>
+	</div>
+</body>
+</html>
