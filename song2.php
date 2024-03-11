fe3e401a9c25c76f9bb304a6fe95a37cf4c6450e
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:32:13 +0800

    song2.php added

---
+++ song2.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Take All The Love</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+	<?php
+
+	echo "<pre>";
+	// TITLE
+	echo "<strong>Take All The Love</strong>";
+	echo "<br>by Arthur Nery";
+	echo "<br><br><br>";
+
+	// VERSE 1
+	echo "<div class='chords'>Dm7\t     Am7    Gm     F      C</div>";
+	echo "You'll be safe in my romance, babe";
+	echo "<br>";
+	echo "<div class='chords'>Dm7  Am7</div>";
+	echo "Label's fading";
+	echo "<br>";
+	echo "<div class='chords'>Gm\t\t            F      C</div>";
+	echo "But I see you cravin' for more";
+	echo "<br>";
+	echo "<div class='chords'>Dm7    Am7</div>";
+	echo "You're denying it";
+	echo "<br>";
+	echo "<div class='chords'>Gm\t       F              C</div>";
+	echo "But you ain't controlling your actions";
+	echo "<br>";
+	echo "<div class='chords'>Dm7   Am7</div>";
+	echo "Keep me guessing";
+	echo "<br>";
+	echo "<div class='chords'>Gm\t\t\tF</div>";
+	echo "But don't keep me waiting for us";
+	echo "<br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>Dm7       Am7</div>";
+	echo "Oh I live to appreciate you";
+	echo "<br>";
+	echo "<div class='chords'> Gm\t\t\t       F    C</div>";
+	echo "Take all the love that I give";
+	echo "<br>";
+	echo "<div class='chords'>Dm7\t      Am7</div>";
+	echo "All that I wish is for you to take me";
+	echo "<br>";
+	echo "<div class='chords'>  Gm\t\t\t                 F C</div>";
+	echo "But I taste the lies on your lips";
+	echo "<br>";
+	echo "<div class='chords'>                 Dm7\t\t\tAm7</div>";
+	echo "I count the days when we're in love";
+	echo "<br>";
+	echo "<div class='chords'>\t\t Gm\t\t\t\tF C</div>";
+	echo "'Cuz I only matter when he's not around";
+	echo "<br>";
+	echo "<div class='chords'>    Dm7\t\t    Am7</div>";
+	echo "Oh but I live to appreciate you";
+	echo "<br>";
+	echo "<div class='chords'> Gm\t\t\t\t    F  C</div>";
+	echo "So take all the love that I give";
+	echo "<br><br>";
+
+	// VERSE 2
+	echo "<div class='chords'>\t  Dm7         Am7                  	        </div>";
+	echo "Got my heart on my sleeve";
+	echo "<br>";
+	echo "<div class='chords'>Gm\t\t   F\t\t C    </div>";
+	echo "I'm beggin' the heavens to be good to me";
+	echo "<br>";
+	echo "<div class='chords'>  Dm7\t       Am7             </div>";
+	echo "Are you obsessive love?";
+	echo "<br>";
+	echo "<div class='chords'>     Gm\t         F           C\t                Dm7     </div>";
+	echo "'Cuz I'm tolerating all of your desires";
+	echo "<br>";
+	echo "<div class='chords'>  Am7t\t\t\t       Gm</div>";
+	echo "I'll keep you wanting me more oh";
+	echo "<br>";
+	echo "<div class='chords'>     F\t\t      C</div>";
+	echo "Leave but I'll keep you comin'";
+	echo "<br>";
+	echo "<div class='chords'>Dm7   Am7\tGm\t\t\t   F    C</div>";
+	echo "I'm sure it's my love that got you suffocated";
+	echo "<br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>Dm7       Am7</div>";
+	echo "Oh I live to appreciate you";
+	echo "<br>";
+	echo "<div class='chords'> Gm\t\t\t       F    C</div>";
+	echo "Take all the love that I give";
+	echo "<br>";
+	echo "<div class='chords'>Dm7\t      Am7</div>";
+	echo "All that I wish is for you to take me";
+	echo "<br>";
+	echo "<div class='chords'>  Gm\t\t\t                 F C</div>";
+	echo "But I taste the lies on your lips";
+	echo "<br>";
+	echo "<div class='chords'>                 Dm7\t\t\tAm7</div>";
+	echo "I count the days when we're in love";
+	echo "<br>";
+	echo "<div class='chords'>\t\t Gm\t\t\t\tF C</div>";
+	echo "'Cuz I only matter when he's not around";
+	echo "<br>";
+	echo "<div class='chords'>    Dm7\t\t    Am7</div>";
+	echo "Oh but I live to appreciate you";
+	echo "<br>";
+	echo "<div class='chords'> Gm\t\t\t\t    F  C</div>";
+	echo "So take all the love that I give";
+	echo "<br><br>";
+
+	// Bridge
+	echo "<div class='chords'>    Dm7\t\t    Am7\t\t\t      </div>";
+	echo "You come over but not too often";
+	echo "<br>";
+	echo "<div class='chords'>Gm\t\t\t  F\t\t   C      </div>";
+	echo "You don't visit 'cuz you got more options ";
+	echo "<div class='chords'>    Dm7\t\t    Am7\t\t\t      </div>";
+	echo "You come over but not too often";
+	echo "<br>";
+	echo "<div class='chords'>Gm\t\t\t  F\t\t   C      </div>";
+	echo "You don't visit 'cuz you got more options ";
+	echo "<div class='chords'>    Dm7\t\t    Am7\t\t\t      </div>";
+	echo "You come over but not too often";
+	echo "<br>";
+	echo "<div class='chords'>Gm\t\t\t  F\t\t   C      </div>";
+	echo "You don't visit 'cuz you got more options ";
+	echo "<br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>Dm7       Am7</div>";
+	echo "Oh I live to appreciate you";
+	echo "<br>";
+	echo "<div class='chords'> Gm\t\t\t       F    C</div>";
+	echo "Take all the love that I give";
+	echo "<br>";
+	echo "<div class='chords'>Dm7\t      Am7</div>";
+	echo "All that I wish is for you to take me";
+	echo "<br>";
+	echo "<div class='chords'>  Gm\t\t\t                 F C</div>";
+	echo "But I taste the lies on your lips";
+	echo "<br>";
+	echo "<div class='chords'>                 Dm7\t\t\tAm7</div>";
+	echo "I count the days when we're in love";
+	echo "<br>";
+	echo "<div class='chords'>\t\t Gm\t\t\t\tF C</div>";
+	echo "'Cuz I only matter when he's not around";
+	echo "<br>";
+	echo "<div class='chords'>    Dm7\t\t    Am7</div>";
+	echo "Oh but I live to appreciate you";
+	echo "<br>";
+	echo "<div class='chords'> Gm\t\t\t\t    F  C</div>";
+	echo "So take all the love that I give";
+	echo "<br><br>";
+	echo "</pre>";
+	?>
+	</div>
+</body>
+</html>
