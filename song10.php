b67553363438adec120792d87fb0381ce47217ba
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:38:12 +0800

    song10.php added

---
+++ song10.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Stay</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+	<?php
+
+	echo "<pre>";
+
+	// Title
+	echo "<strong>Stay</strong>";
+	echo "\nBy: The Kid Laroi & Justin Bieber";
+
+	// Chorus
+	echo "\n\n\n[Chorus]";
+	echo "<div class='chords'>Fmaj7     Fmaj9 Fmaj7       G                             </div>";
+	echo "I do the same thing I told you that I never would";
+	echo "<div class='chords'>   Am                            Em                         </div>";
+	echo "I told you I'd change, even when I knew I never could";
+	echo "<div class='chords'>Fmaj7       Fmaj9    Fmaj7     G                          </div>";
+	echo "Know that I can't find nobody else as good as you";
+	echo "<div class='chords'>  Am                     Em                           </div>";
+	echo "I need you to stay, need you to stay, hey (Oh)";
+
+	// Verse
+	echo "\n\n\n[Verse]";
+	echo "<div class='chords'>Fmaj7        Fmaj9       G                  </div>";
+	echo "I get drunk, wake up, I'm wasted still";
+	echo "<div class='chords'>Am                        Em                </div>";
+	echo "I realize the time that I wasted here";
+	echo "<div class='chords'>Fmaj7         Fmaj9                   G         </div>";
+	echo "I feel like you can't feel the way I feel";
+	echo "<div class='chords'>    Am                   Em                           </div>";
+	echo "Oh, I'll be fucked up if you can't be right here";
+	echo "\n\n<div class='chords'>Fmaj7        Fmaj9          Fmaj7           </div>";
+	echo "Oh, ooh-whoa (Oh, ooh-whoa, ooh-whoa)";
+	echo "<div class='chords'>Am                Em                        </div>";
+	echo "Oh, ooh-whoa (Oh, ooh-whoa, ooh-whoa)";
+	echo "<div class='chords'>Fmaj7        Fmaj9          Fmaj7            </div>";
+	echo "Oh, ooh-whoa (Oh, ooh-whoa, ooh-whoa)";
+	echo "<div class='chords'>       G                       N.C.                    </div>";
+	echo "Oh, I'll be fucked up if you can't be right here";
+
+	// Chorus
+	echo "\n\n\n[Chorus]";
+	echo "<div class='chords'>Fmaj7      Fmaj9 Fmaj7        G                            </div>";
+	echo "I do the same thing I told you that I never would";
+	echo "<div class='chords'>   Am                      Em                               </div>";
+	echo "I told you I'd change, even when I knew I never could";
+	echo "<div class='chords'>Fmaj7      Fmaj9 Fmaj7        G                            </div>";
+	echo "Know that I can't find nobody else as good as you";
+	echo "<div class='chords'>  Am                          Em                          </div>";
+	echo "I need you to stay, need you to stay, hey";
+	echo "<div class='chords'>Fmaj7      Fmaj9 Fmaj7        G                            </div>";
+	echo "I do the same thing I told you that I never would";
+	echo "<div class='chords'>   Am                       Em                                 </div>";
+	echo "I told you I'd change, even when I knew I never could";
+	echo "<div class='chords'>Fmaj7      Fmaj9 Fmaj7        G                            </div>";
+	echo "Know that I can't find nobody else as good as you";
+	echo "<div class='chords'>  Am                Em                         </div>";
+	echo "I need you to stay, need you to stay, hey";
+
+	// Verse
+	echo "\n\n\n[Verse]";
+	echo "<div class='chords'>N.C.           </div>";
+	echo "When I'm away from you, I miss your touch (Ooh)";
+	echo "<div class='chords'>Am                             Em</div>";
+	echo "You're the reason I believe in love";
+	echo "<div class='chords'>Fmaj7                         G</div>";
+	echo "It's been difficult for me to trust (Ooh)";
+	echo "<div class='chords'>    Am                   Em</div>";
+	echo "And I'm afraid that I'ma fuck it up";
+	echo "<div class='chords'>Fmaj7                   G</div>";
+	echo "Ain't no way that I can leave you stranded";
+	echo "<div class='chords'>Am                             Em</div>";
+	echo "'Cause you ain't never left me empty-handed";
+	echo "<div class='chords'>Fmaj7               G                          Am</div>";
+	echo "And you know that I know that I can't live without you";
+	echo "<div class='chords'>          Em</div>";
+	echo "So, baby, stay";
+	echo "\n\n\n<div class='chords'>Fmaj7        Fmaj9          Fmaj7           </div>";
+	echo "Oh, ooh-whoa (Oh, ooh-whoa, ooh-whoa)";
+	echo "<div class='chords'>Am                Em                        </div>";
+	echo "Oh, ooh-whoa (Oh, ooh-whoa, ooh-whoa)";
+	echo "<div class='chords'>Fmaj7        Fmaj9          Fmaj7            </div>";
+	echo "Oh, ooh-whoa (Oh, ooh-whoa, ooh-whoa)";
+	echo "<div class='chords'>       G                       N.C.                    </div>";
+	echo "Oh, I'll be fucked up if you can't be right here";
+
+	// Chorus
+	echo "\n\n\n[Chorus]";
+echo "<div class='chords'>Fmaj7      Fmaj9 Fmaj7        G                            </div>";
+	echo "I do the same thing I told you that I never would";
+	echo "<div class='chords'>   Am                      Em                               </div>";
+	echo "I told you I'd change, even when I knew I never could";
+	echo "<div class='chords'>Fmaj7      Fmaj9 Fmaj7        G                            </div>";
+	echo "Know that I can't find nobody else as good as you";
+	echo "<div class='chords'>  Am                          Em                          </div>";
+	echo "I need you to stay, need you to stay, hey";
+	echo "<div class='chords'>Fmaj7      Fmaj9 Fmaj7        G                            </div>";
+	echo "I do the same thing I told you that I never would";
+	echo "<div class='chords'>   Am                       Em                                 </div>";
+	echo "I told you I'd change, even when I knew I never could";
+	echo "<div class='chords'>Fmaj7      Fmaj9 Fmaj7        G                            </div>";
+	echo "Know that I can't find nobody else as good as you";
+	echo "<div class='chords'>  Am                Em                         </div>";
+	echo "I need you to stay, need you to stay, hey";
+
+	// Outro
+	echo "\n\n\n[Outro]";
+	echo "<div class='chords'>Fmaj7 G Am Em</div>";
+	echo "<div class='chords'>Fmaj7 G</div>";
+	echo "Whoa-oh";
+	echo "<div class='chords'>  Am                            Em</div>";
+	echo "    I need you to stay, need you to stay, hey";
+	?>
+	</div>
+</body>
+</html>
