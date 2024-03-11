e67ebf1568a7cf6895a6cb257659b1ce62f95484
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:35:58 +0800

    song8/php added

---
+++ song8.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Synesthesia</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+	<?php
+
+	echo "<pre>";
+
+	// Title
+	echo "<strong>Synesthesia</strong>";
+	echo "\nBy: Mayonnaise";
+
+	// Intro
+	echo "\n\n[Intro]";
+	echo "<div class='chords'>G D/F# Em Cadd9 x2</div>";
+
+	// Verse 1
+	echo "\n\n[Verse 1]";
+	echo "<div class='chords'>Em   D    Cadd9   </div>";
+	echo "Save your smile";
+	echo "<div class='chords'>Em         D              Cadd9   </div>";
+	echo "Everything fades through time";
+	echo "<div class='chords'>    Em   D    Cadd9</div>";
+	echo "I'm lost for words";
+	echo "<div class='chords'>Em         D            Cadd9</div>";
+	echo "Endlessly waiting for you";
+	echo "\n\n<div class='chords'>Em   D     Cadd9</div>";
+	echo "Stay with me";
+	echo "<div class='chords'>Em          D           Cadd9</div>";
+	echo "Yes, I know this cannot be";
+	echo "<div class='chords'>    Em   D   Cadd9</div>";
+	echo "As morning comes";
+	echo "<div class='chords'>     Em              D           Cadd9</div>";
+	echo "I'll say goodbye to you when I'm done";
+	echo "<div class='chords'>            D</div>";
+	echo "Through the sun";
+
+	// Chorus
+	echo "\n\n[Chorus]";
+	echo "<div class='chords'>        G                D/F#</div>";
+	echo "Because I'm waiting for you";
+	echo "<div class='chords'>             Em                  Cadd9</div>";
+	echo "Waiting for this dream to come true";
+	echo "<div class='chords'>                  G</div>";
+	echo "Just to be with you";
+	echo "<div class='chords'>          D/F#</div>";
+	echo "And if I die";
+	echo "<div class='chords'>                Em</div>";
+	echo "Remember these lines";
+	echo "<div class='chords'>           Cadd9               G D/F#</div>";
+	echo "I'm always here, guarding your life";
+	echo "<div class='chords'>              Em D Cadd9</div>";
+	echo "Guarding your life";
+
+	// Verse 2
+	echo "\n\n[Verse 2]";
+	echo "<div class='chords'>Em D  Cadd9 </div>";
+	echo "I am yours";
+	echo "<div class='chords'>Em                  D               Cadd9     </div>";
+	echo "And I'm completely trapped in your soul";
+	echo "<div class='chords'>Em      D     Cadd9   </div>";
+	echo "Dazed and confused";
+	echo "<div class='chords'>Em          D              C          </div>";
+	echo "Swept away with your whole world";
+	echo "\n\n<div class='chords'>       Em       D  C</div>";
+	echo "'Cause you're my star";
+	echo "<div class='chords'>Em           D            C</div>";
+	echo "Invincible, haunting and far";
+	echo "<div class='chords'>Em     D     C</div>";
+	echo "Grace under fire";
+	echo "<div class='chords'>Em            D          C           D</div>";
+	echo "Something is deep in my heart, in my heart";
+
+	// Chorus
+	echo "\n\n[Chorus]";
+	echo "<div class='chords'>        G                D/F#</div>";
+	echo "Because I'm waiting for you";
+	echo "<div class='chords'>              Em                 Cadd9</div>";
+	echo "Waiting for this dream to come true";
+	echo "<div class='chords'>                  G</div>";
+	echo "Just to be with you";
+	echo "<div class='chords'>         D/F#</div>";
+	echo "And if I die";
+	echo "<div class='chords'>                Em</div>";
+	echo "Remember these lines";
+	echo "<div class='chords'>            Cadd9</div>";
+	echo "I'm always here, guarding you";
+
+	// Bridge
+	echo "\n\n[Bridge]";
+	echo "<div class='chords'>Am      Bm          C      </div>";
+	echo "Slowly falling into you";
+	echo "<div class='chords'>                      Bm</div>";
+	echo "I'm obsessed with the fact that I'm with you";
+	echo "<div class='chords'>Am      Bm               C</div>";
+	echo "I can't breathe without you";
+	echo "<div class='chords'>Cm</div>";
+	echo "Oooooohhhhh Ohhhhhh";
+
+	// Chorus
+	echo "\n\n[Chorus]";
+	echo "<div class='chords'>G                D/F#</div>";
+	echo "I'm waiting for you";
+	echo "<div class='chords'>              Em                 Cadd9</div>";
+	echo "Waiting for this dream to come true";
+	echo "<div class='chords'>                  G</div>";
+	echo "Just to be with you";
+	echo "<div class='chords'>          D/F#</div>";
+	echo "And if I die";
+	echo "<div class='chords'>                    Em</div>";
+	echo "Remember these lines";
+	echo "<div class='chords'>            Cadd9                 G D/F# Em Cadd9</div>";
+	echo "I'm always here, guarding your life";
+	echo "<div class='chords'>G D/F# Em D Cadd9</div>";
+	echo "ohhhhhhooooohoh";
+	?>
+
+
+	</div>
+</body>
+</html>
