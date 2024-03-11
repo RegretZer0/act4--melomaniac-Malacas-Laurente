10fa5019b2a1b2dd4b961497789ac5ca7daf56cc
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:30:38 +0800

    index.php added

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Try</title>
+</head>
+<style>
+	body{
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 125vh;
+		background: url(homepage-bg.jpg);
+		background-position: center;
+		background-size: cover;
+	}
+
+	.title{
+		align-items: center;
+		display: flex;
+	}
+
+	h1{
+		width: 100%;
+		position: absolute;
+		text-align: center;
+		top: 5%;
+		color: white;
+		font-size: 60px;
+
+	}
+
+	h2{
+   		width: 100%;
+   		position: absolute;
+   	 	text-align: center;
+    	top: 10%;
+    	left: 0;
+    	right: 0;
+    	margin-top: 7%;
+    	color: white;
+	}
+
+	.songs{
+		align-items: center;
+		background-origin: border-box;
+		widows: 100%;
+		text-align: center;
+		margin-top: 20%;
+		padd
+	}
+	.album{
+		width: 15%; /* Adjusted width to fit 5 columns */
+    	margin: 1%; /* Added margin to separate the albums */
+ 	  	box-sizing: border-box;
+   		display: inline-block; /* Display albums in line */
+   		vertical-align: top;
+ 	
+	}	
+
+</style>
+<body>
+	<div class="title">
+		<h1>YoujiTube</h1>
+		<h2>You're daily dose of Music</h2>
+	</div>
+
+
+	<div class="songs">
+		<a href="song1.php"><img src="akon.jpg" class="album">
+		<a href="song2.php"><img src="arthur.jpg" class="album">
+		<a href="song3.php"><img src="ysabelle.jpg" class="album">
+		<a href="song4.php"><img src="emman.jpg" class="album">
+		<a href="song5.php"><img src="himala2.jpg" class="album">
+		<a href="song6.php"><img src="december.jpg" class="album">
+		<a href="song7.php"><img src="silent.jpg" class="album">
+		<a href="song8.php"><img src="synesthesia.jpg" class="album">
+		<a href="song9.php"><img src="dahan.jpg" class="album">
+		<a href="song10.php"><img src="stay.jpg" class="album">
+	</div>
+
+	</body>
+</html>
