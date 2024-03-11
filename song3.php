46732df2e8e5eb520ad8b05595e904571820a972
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:32:46 +0800

    song3.php added

---
+++ song3.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>I Like You So Much, You'll Know It (A Love So Beautiful OST English Cover)</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+	<?php
+
+	echo "<pre>";
+	// TITLE
+	echo "<strong>I Like You So Much, You'll Know It (A Love So Beautiful OST English Cover)</strong>";
+	echo "<br>";
+	echo "by Ysabelle Cuevas";
+	echo "<br><br><br>";
+
+	// VERSE 1
+	echo "<div class='chords'>  D\t\t\t\t    Bm        </div>";
+	echo "I like your eyes, you look away when you pretend not to care";
+	echo "<br>";
+	echo "<div class='chords'>Em\t\t\t\t    A        </div>";
+	echo "I like the dimples on the corners of the smile that you wear";
+	echo "<br>";
+	echo "<div class='chords'>\tF#m\t\t\t\t   B           </div>";
+	echo "I like you more, the world may know but don't be scared";
+	echo "<br>";
+	echo "<div class='chords'>Em\t\t\t  A</div>";
+	echo "'Cause I'm falling deeper, baby be prepared";
+	echo "<br><br>";
+
+	echo "<div class='chords'>  D\t\t\t\t    Bm        </div>";
+	echo "I like your shirt, I like your fingers, love the way that you smell";
+	echo "<br>";
+	echo "<div class='chords'> Em\t\t\t\t    A        </div>";
+	echo "To be your favorite jacket, just so I could always be near";
+	echo "<br>";
+	echo "<div class='chords'>\tF#m\t\t\t\t   B           </div>";
+	echo "To be your favorite jacket, just so I could always be near";
+	echo "<br>";
+	echo "<div class='chords'>Em\t\t\t  A</div>";
+	echo "But after all this time, I hope you wait and see";
+	echo "<br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>   D\t\t\t\tA\t\t</div>";
+	echo "Love you every minute, every second";
+	echo "<br>";
+	echo "<div class='chords'>   Bm\t\t\t\t F#m\t\t</div>";
+	echo "Love you everywhere and any moment";
+	echo "<br>";
+	echo "<div class='chords'>G\t\t\t D\t\t\t  </div>";
+	echo "Always and forever I know I can't quit you";
+	echo "<br>";
+	echo "<div class='chords'>Em\t\t\t  A\t\t\t  </div>";
+	echo "'Cause baby you're the one, I don't know how";
+	echo "<br>";
+	echo "<div class='chords'>D\t\t\t\t A\t\t   </div>";
+	echo "I love you 'til the last of snow disappears";
+	echo "<br>";
+	echo "<div class='chords'>  Bm\t\t\t\t F#m\t\t   </div>";
+	echo "Love you 'til a rainy day becomes clear";
+	echo "<br>";
+	echo "<div class='chords'>    G\t\t\t\tD\t\t\t\t </div>";
+	echo "Never knew a love like this, now I can't let go";
+	echo "<br>";
+	echo "<div class='chords'>Em\t\t    A\t\t\t     </div>";
+	echo "I'm in love with you, and now you know";
+	echo "<br>";
+	echo "<div class='chords'>GA F#m Bm Em A D</div>";
+	echo "<br>";
+
+
+	// VERSE 2
+	echo "<div class='chords'> D\t\t\t\t    Bm\t\t\t</div>";
+	echo "I like the way you try so hard when you play ball with your friends";
+	echo "<br>";
+	echo "<div class='chords'>Em\t\t\t\t       A\t</div>";
+	echo "I like the way you hit the notes, in every song you're shining";
+	echo "<br>";
+	echo "<div class='chords'>    F#m\t\t\t\t           B</div>";
+	echo "I love the little things, like when you're unaware";
+	echo "<br>";
+	echo "<div class='chords'>     Em\t\t\t\t      A</div>";
+	echo "I catch you steal a glance and smile so perfectly";
+	echo "<br><br>";
+
+	// BRIDGE
+	echo "<div class='chords'>Em\t\t\t   A\t\t\t  </div>";
+	echo "Though sometimes when life brings me down";
+	echo "<br>";
+	echo "<div class='chords'>  Fm#\t\t\t Bm\t</div>";
+	echo "You're the cure my love";
+	echo "<br>";
+	echo "<div class='chords'>G\t  </div>";
+	echo "In a bad rainy day";
+	echo "<br>";
+	echo "<div class='chords'>   Em\t\t\t     A\t</div>";
+	echo "You take all the worries away";
+	echo "<br><br>";
+
+	// CHORUS
+	echo "<div class='chords'>   D\t\t\t\tA\t\t</div>";
+	echo "Love you every minute, every second";
+	echo "<br>";
+	echo "<div class='chords'>   Bm\t\t\t\t F#m\t\t</div>";
+	echo "Love you everywhere and any moment";
+	echo "<br>";
+	echo "<div class='chords'>G\t\t\t D\t\t\t  </div>";
+	echo "Always and forever I know I can't quit you";
+	echo "<br>";
+	echo "<div class='chords'>Em\t\t\t  A\t\t\t  </div>";
+	echo "'Cause baby you're the one, I don't know how";
+	echo "<br>";
+	echo "<div class='chords'>D\t\t\t\t A\t\t   </div>";
+	echo "I love you 'til the last of snow disappears";
+	echo "<br>";
+	echo "<div class='chords'>  Bm\t\t\t\t F#m\t\t   </div>";
+	echo "Love you 'til a rainy day becomes clear";
+	echo "<br>";
+	echo "<div class='chords'>    G\t\t\t\tD\t\t\t\t </div>";
+	echo "Never knew a love like this, now I can't let go";
+	echo "<br>";
+	echo "<div class='chords'>Em\t\t    A\t\t\tD    </div>";
+	echo "I'm in love with you, and now you know";
+	echo "<div class='chords'>Em\t\t    A\t\t\tD    </div>";
+	echo "I'm in love with you, and now you know";
+
+	echo "</pre>";
+	?>
+	</div>
+</body>
+</html>
