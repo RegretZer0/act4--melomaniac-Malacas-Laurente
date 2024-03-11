443540cce1e35ad77a6ab2f4c4623e9bef232c3e
malacasmarc16@gmail.com <malacasmarc16@gmail.com>
2024-03-11 17:34:47 +0800

    song6.php added

---
+++ song6.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Kahit Di Mo Alam</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			background: linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.0)), url(bgs.jpg);
+			background-size: cover;
+			background-position: center;
+
+
+			}
+			.alignment{
+				font-size: 18px;
+				text-align: center;
+				color: #ffff;
+			}
+			.chords {
+				color: orange;
+				font-size: 15px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="alignment">
+	<?php
+	echo "<pre>";
+
+	// Title
+	echo "<strong>Kahit Di Mo Alam</strong>";
+	echo "\nBy: December Avenue";
+
+	// Intro
+	echo "\n\n[Intro]";
+	echo "<div class='chords'>AAsusA</div>";
+
+	// Verse
+	echo "\n\n[Verse]";
+	echo "<div class='chords'>A\t\t\t</div>";
+	echo "Ipikit mo man ang iyong mata";
+	echo "<div class='chords'>Asus\t\t</div>";
+	echo "Di pa rin naman mag-iiba";
+	echo "<div class='chords'>    A/D\t\t\t\t\tD9</div>";
+	echo "Nabalutan ng poot ang puso mo";
+	echo "<div class='chords'>A\t\t\t  </div>";
+	echo "Tila malimit kang ngumiti ngayon";
+	echo "<div class='chords'> Asus\t\t\t</div>";
+	echo "Di ka rin naman ganyan noon";
+	echo "<div class='chords'>    A/D\t\t\t\t\tD9</div>";
+	echo "Naubusan ng tibok ang puso mo";
+
+	// Refrain
+	echo "\n\n\n[Refrain]";
+	echo "<div class='chords'>  F#m\t\t\t</div>";
+	echo "Kulang na ba ang mga ulap";
+	echo "<div class='chords'> E\t\t\t</div>";
+	echo "Sa langit at buwan";
+	echo "<div class='chords'>  D9\t\t\t\tE7  </div>";
+	echo "Di ka na babalik sa lilim ng ulan";
+
+	// Chorus
+	echo "\n\n\n[Chorus]";
+	echo "<div class='chords'>   A\t\t\t</div>";
+	echo "Sa bawat saglit";
+	echo "<div class='chords'>\t\tAM7 </div>";
+	echo "Handang masaktan";
+	echo "<div class='chords'>\t\t\tD9E7</div>";
+	echo "Kahit di mo alam";
+	echo "<div class='chords'>\t\tA</div>";
+	echo "Subukang muli";
+	echo "<div class='chords'>\t\tAM7 </div>";
+	echo "At pagbigyan";
+	echo "<div class='chords'>\t\t\tD9E7   </div>";
+	echo "Ang ating nakaraan";
+	echo "\n\nKahit di mo na alam(pause after E7)";
+
+	// Instrumental
+	echo "\n\n[Instrumental]";
+	echo "<div class='chords'>A-Asus-A/D-D9 2x</div>";
+
+	// Verse
+	echo "\n\n[Verse]";
+	echo "<div class='chords'>   A\t\t\t\t</div>";
+	echo "Ipikit mo na ang iyong mata";
+	echo "<div class='chords'>Asus\t\t   </div>";
+	echo "Ang nakaraa'y limutin na";
+	echo "<div class='chords'>  A/D\t\t\t\t D9</div>";
+	echo "Umaasang di ka na mawawala";
+	echo "<div class='chords'>A\t\t\t\t  </div>";
+	echo "Sadyang mahirap lang ngumiti ngayon";
+	echo "<div class='chords'> Asus\t\t\t</div>";
+	echo "Minahal kita mula noon";
+	echo "<div class='chords'>A/D\t\t\t\t D9</div>";
+	echo "Ibalik na ang tibok ng puso mo";
+
+	// Refrain
+	echo "\n\n\n[Refrain]";
+	echo "<div class='chords'>  F#m\t\t\t</div>";
+	echo "Kulang na ba ang mga ulap";
+	echo "<div class='chords'> E\t\t\t</div>";
+	echo "Sa langit at buwan";
+	echo "<div class='chords'>  D9\t\t\t\tE7  </div>";
+	echo "Di ka na babalik sa lilim ng ulan";
+
+	// Chorus
+	echo "\n\n\n[Chorus]";
+	echo "<div class='chords'>   A\t\t\t</div>";
+	echo "Sa bawat saglit";
+	echo "<div class='chords'>\t\tAM7 </div>";
+	echo "Handang masaktan";
+	echo "<div class='chords'>\t\t\tD9E7</div>";
+	echo "Kahit di mo alam";
+	echo "<div class='chords'>\t\tA</div>";
+	echo "Subukang muli";
+	echo "<div class='chords'>\t\tAM7 </div>";
+	echo "At pagbigyan";
+	echo "<div class='chords'>\t\t\tD9E7   </div>";
+	echo "Ang ating nakaraann";
+	echo "<div class='chords'>AAsusA/DD9\t\t</div>";
+	echo "Kahit di mo na alam";
+	echo "\n\nKahit di mo na...";
+
+	// Instrumental
+	echo "\n\n\n[Instrumental]";
+	echo "<div class='chords'>F#m E D9 E7 2x</div>";
+
+	// Chorus
+	echo "\n\n\n[Chorus]";
+	echo "<div class='chords'>   A\t\t\t</div>";
+	echo "Sa bawat saglit";
+	echo "<div class='chords'>\t\tAM7 </div>";
+	echo "Handang masaktan";
+	echo "<div class='chords'>\t\t\tD9E7</div>";
+	echo "Kahit di mo alam";
+	echo "<div class='chords'>\t\tA</div>";
+	echo "Subukang muli";
+	echo "<div class='chords'>\t\tAM7 </div>";
+	echo "At pagbigyan";
+	echo "<div class='chords'>\t\t\tD9E7   </div>";
+	echo "Ang ating nakaraann";
+	echo "<div class='chords'>\t\t\t  AAsus</div>";
+	echo "Kahit di mo na alam";
+	echo "\n(Ipikit mo man ang iyong mata)";
+	echo "\n(Di pa rin naman mag-iiba)";
+	echo "<div class='chords'>\t\t\t  A/D D9</div>";
+	echo "Kahit di mo na alam";
+	echo "\n(Nabalutan ng poot ang puso mo)";
+	echo "<div class='chords'>\t\t\t  AAsus</div>";
+	echo "Kahit di mo na alam";
+	echo "\n(Ipikit mo na ang ‘yong mata)";
+	echo "\n(Ang nakaraa'y limutin na)";
+	echo "<div class='chords'>\t\t\t  A/D D9</div>";
+	echo "Kahit di mo na...";
+	echo "\n(Umaasang di ka na mawawala)";
+	echo "\nKahit di mo na...";
+	echo "<div class='chords'>\t\t  A/D D9</div>";
+	echo "Kahit di mo na alam";
+	?>
+	</div>
+</body>
+</html>
